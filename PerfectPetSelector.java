f0efcd8d4bdf0b220b351c8277bc70814c563659
Arrune <alpachay45@gmail.com>
2025-10-27 01:38:24 +0000

    Revised Version

--- PerfectPetSelector.java
+++ PerfectPetSelector.java
@@ -16,67 +16,39 @@ public class PerfectPetSelector {
         System.out.println("Enter your favorite color (either red, blue, or green).");
         String color  = obj.nextLine().toLowerCase(); // Read users favorite color
         
-        //error messages for idiot proofing
-        //Note: I'm struggling to get this to work.
-        String errorMessage1 = "It seems that you did not choose a season or made a typo. Please type your favorite season with correct spelling";
-        String errorMessage2 = "It seems that you may have wrote the wrong color. Please choose between red, blue, or green and answer the questions with proper spelling";
+        //variables set up
+        boolean startsWithConsonant = "aeiou".indexOf(name.charAt(0)) == -1; // Checks if the users name starts with a consonant
+        boolean startsWithVowel = !startsWithConsonant;
+        String pet = "pet rock"; // if the person does not qualify for any, they recieve a pet rock
         
-        if (color.equals("blue")){
-            if (season.equals("spring")){
-            System.out.println("Your perfect pet is an ostrich");
-            }
-            else if ("aeiou".indexOf(name.charAt(0)) == -1 && (!season.equals("summer")) && (!season.equals("fall"))){
-            System.out.println("Your perfect pet is an axolotl");
-            }
-            else if (season.equals("fall")){
-            System.out.println("Your perfect pet is an alligator");
-            }
-            else {
-            System.out.println("Your perfect pet is a rock");
-            }
-
+        //If and else if statements for finding the right pet
+        if (color.equals("blue") && season.equals ("spring")) {
+            pet = "ostrich";
         }
-        else if (color.equals("green")){
-            if ("aeiou".indexOf(name.charAt(0)) == -1 && (season.equals("winter"))){
-            System.out.println("Your perfect pet is a giraffe");
-            }
-            else if (!season.equals("fall")){
-            System.out.println("Your perfect pet is a dog");
-            }
-            else if (season.equals("summer")){
-            System.out.println("Your perfect pet is a pony");
-            }
-            else {
-            System.out.println("Your perfect pet is a rock");
-            }
+        else if (color.equals("green") && startsWithConsonant && season.equals("winter")){
+            pet = "giraffe";
         }
-        else if (color.equals("red")){
-            if ("aeiou".indexOf(name.charAt(0)) == -1 ){
-            System.out.println("Your perfect pet is a porcupine");
-            }
-            else if ("aeiou".indexOf(name.charAt(0)) != -1){
-            System.out.println("Your perfect pet is a panda");
-            }
-            else if (color.equals("summer")){
-            System.out.println("Your perfect pet is a pony");
-            }
-            else {
-                System.out.println("Your perfect pet is rock");
-            }
+        else if (color.equals("blue") && season.equals("fall")){
+            pet = "alligator";
         }
-        else if (!color.equals("red") && !color.equals("green") && !color.equals("blue")){
-            System.out.println(errorMessage2);
-            System.out.println("Enter your favorite color (either red, blue, or green).");
-            color  = obj.nextLine().toLowerCase();
-
+        else if (color.equals("red") && startsWithVowel){
+            pet = "porcupine";
         }
-        else if (!season.equals("summer") && !season.equals("spring") && !season.equals("fall") && !season.equals("winter")) {
-            System.out.println(errorMessage1);
-            System.out.println("Enter your favorite season.");
-            season = obj.nextLine().toLowerCase();
+        else if (color.equals("red") && startsWithConsonant){
+            pet = "panda";
         }
-        else {
-            System.out.println("Your perfect pet is rock");
+        else if (color.equals ("green") && !season.equals("fall")){
+            pet = "dog";
         }
+        else if (startsWithConsonant && color.equals("blue") && !season.equals("summer") && !season.equals("fall")){
+            pet = "axolotl";
+        }
+        else if (season.equals("summer") && startsWithVowel){
+            pet = "pony";
+        }
+        
+        //Prints the result
+        System.out.println("Your perfect pet is a " + pet);
+        obj.close();
     }
 }
